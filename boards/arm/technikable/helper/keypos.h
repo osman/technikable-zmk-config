f65b0978a2066076bca51f2d330765299f482a03
Osman Currim <osman.currim@rci.rogers.com>
2023-04-08 06:11:00 +0000

    move thumbs in

--- boards/arm/technikable/helper/keypos.h
+++ boards/arm/technikable/helper/keypos.h
@@ -3,7 +3,7 @@
 /*    0  1  2  3  4 | 5   6|  7  8  9 10 11   ||   LT5 LT4 LT3 LT2 LT1 LT0 | RT0 RT1 RT2 RT3 RT4 RT5   */
 /*   12 13 14 15 16 |17  18| 19 20 21 22 23   ||   LM5 LM4 LM3 LM2 LM1 LM0 | RM0 RM1 RM2 RM3 RM4 RM5   */
 /*   24 25 26 27 28 |29  30| 31 32 33 34 35   ||   LB5 LB4 LB3 LB2 LB1 LB0 | RB0 RB1 RB2 RB3 RB4 RB5   */
-/*         36 37 38  __39__  40 41 42         ||           LH2 LH1 LH0 ___DMH___ RH0 RH1 RH2           */
+/*         36 37 38 |39  40| 41 42 43         ||           DL3 DL2 LH1 LH0 | RH0 RH1 DR2 DR3           */
 
 // Top Row
 #define LT5  0
@@ -48,10 +48,11 @@
 #define RB5 35
 
 // Thumb Keys
-#define LH2 36
-#define LH1 37
-#define LH0 38
-#define DMH 39
+#define DL3 36
+#define DL2 37
+#define LH1 38
+#define LH0 39
 #define RH0 40
 #define RH1 41
-#define RH2 42
+#define DR2 42
+#define DR3 43
