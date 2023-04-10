df2d5565887c891ef67256611bd43f63c0571c68
Osman Currim <osman.currim@rci.rogers.com>
2023-04-10 07:58:08 +0000

    split adjust layer to sym2 and admin

--- boards/arm/technikable/helper/keypos_mit.h
+++ boards/arm/technikable/helper/keypos_mit.h
@@ -1,9 +1,9 @@
 #pragma once
 
-/*    0  1  2  3  4 | 5   6|  7  8  9 10 11   ||   LT5 LT4 LT3 LT2 LT1 LT0 |   | RT0 RT1 RT2 RT3 RT4 RT5   */
-/*   12 13 14 15 16 |17  18| 19 20 21 22 23   ||   LM5 LM4 LM3 LM2 LM1 LM0 |   | RM0 RM1 RM2 RM3 RM4 RM5   */
-/*   24 25 26 27 28 |29  30| 31 32 33 34 35   ||   LB5 LB4 LB3 LB2 LB1 LB0 |   | RB0 RB1 RB2 RB3 RB4 RB5   */
-/*         36 37 38 | _39_ | 40 41 42         ||               DL2 LH1 LH0  BTM  RH0 RH1 DR2               */
+/*    0  1  2  3  4  5 ||  6  7  8  9 10 11   ||    LT5 LT4 LT3 LT2 LT1 LT0 | RT0 RT1 RT2 RT3 RT4 RT5   */
+/*   12 13 14 15 16 17 || 18 19 20 21 22 23   ||    LM5 LM4 LM3 LM2 LM1 LM0 | RM0 RM1 RM2 RM3 RM4 RM5   */
+/*   24 25 26 27 28 29 || 30 31 32 33 34 35   ||    LB5 LB4 LB3 LB2 LB1 LB0 | RB0 RB1 RB2 RB3 RB4 RB5   */
+/*       36 37 38 | ___39___ | 40 41 42       ||          DL2 LH1 LH0 | ____BTM____ | RH0 RH1 DR2       */
 
 // Top Row
 #define LT5  0
